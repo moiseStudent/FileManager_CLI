78064c79a30a5d83379388905e66e3f6d34bf4b7
pinguino <luisgatuno555@gmail.com>
2024-12-03 01:58:48 -0400

    checkpoint

--- cli.py
+++ cli.py
@@ -1,3 +1,16 @@
-"""
-*This file will be used to develop the commands with click
-"""
+from InquirerPy import prompt
+
+questions = [
+    {"type": "input", "message": "What's your name:", "name": "name"},
+    {
+        "type": "list",
+        "message": "What's your favourite programming language:",
+        "choices": ["Go", "Python", "Rust", "JavaScript"],
+    },
+    {"type": "confirm", "message": "do you want to delete this file ???"},
+]
+result = prompt(questions)
+name = result["name"]
+fav_lang = result[1]
+confirm = result[2]
+print(result)
--- fileManager.py
+++ fileManager.py
@@ -1,31 +1,44 @@
 import os
 import shutil
+from send2trash import send2trash
+
+# from prompt_toolkit.shortcuts import yes_no_dialog
+from InquirerPy import prompt, inquirer
+
 
 class FileManager:
     def __init__(self) -> None:
         pass
-    
+
     def organize_files(self) -> None:
 
         documents_type = [
-            'images',
-            'documents',
-            'audios',
-            'videos',
-            'compressed',
-            'executable',
-            'dev'
+            "images",
+            "documents",
+            "audios",
+            "videos",
+            "compressed",
+            "executable",
+            "dev",
         ]
 
         ### Extensions Dictionary
         extensions = {
-            documents_type[0]:['.jpg', '.jpeg', '.png', '.gif', '.tiff', '.bmp', '.svg'],
-            documents_type[1]:['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt'],
-            documents_type[2]:['.mp3', '.wav', '.aac', '.flac'],
-            documents_type[3]:['.mp4', '.avi', '.mkv', '.mov'],
-            documents_type[4]:['.zip', '.rar'],
-            documents_type[5]:['.exe', '.bat', '.deb'],
-            documents_type[6]:['.html']
+            documents_type[0]: [
+                ".jpg",
+                ".jpeg",
+                ".png",
+                ".gif",
+                ".tiff",
+                ".bmp",
+                ".svg",
+            ],
+            documents_type[1]: [".doc", ".docx", ".pdf", ".txt", ".rtf", ".odt"],
+            documents_type[2]: [".mp3", ".wav", ".aac", ".flac"],
+            documents_type[3]: [".mp4", ".avi", ".mkv", ".mov"],
+            documents_type[4]: [".zip", ".rar"],
+            documents_type[5]: [".exe", ".bat", ".deb"],
+            documents_type[6]: [".html"],
         }
 
         ### Getting list the files into the curret directory
@@ -33,17 +46,20 @@ class FileManager:
 
         for file in files:
             ### Check that it is not a hidden file.
-            if not file.startswith('.'):
-                if os.path.splitext(file)[1] == '': ### Chekc that it's not a folder.
+            if not file.startswith("."):
+                if os.path.splitext(file)[1] == "":  ### Chekc that it's not a folder.
                     pass
-                
+
                 else:
                     search = False
                     i = 0
 
                     while search == False and i <= 6:
                         ### Search the extension into the dict.
-                        if str(os.path.splitext(file)[1]) in extensions[documents_type[i]]:
+                        if (
+                            str(os.path.splitext(file)[1])
+                            in extensions[documents_type[i]]
+                        ):
                             """
                             When it is True, mean that it has found the extension on
                             the current list, then Makes the folder if not exits and
@@ -59,8 +75,25 @@ class FileManager:
                             print("Guardar en sin clasificar")
 
                             search = False
-                            i += 1                        
+                            i += 1
 
-###! Ejecucio unicamente de prueba - It's only for a test            
+    def DeleteFile(self, path):
+        try:
+            fileStats = os.stat(
+                path
+            )  # show file info before delete the file in the delete dialog of prompt_toolkit
+
+            result = inquirer.confirm(
+                f"do you want delete this file => fileName= {os.path.basename(path)}, file weight= {fileStats.st_size} Bytes ?",
+                default=False,
+            ).execute()
+            if result:
+                send2trash(path)
+        except FileNotFoundError:
+            print("Error: El archivo que deseas eleminar no existe")
+
+
+###! Ejecucio unicamente de prueba - It's only for a test
 manager = FileManager()
-manager.organize_files()
+statInfo = manager.DeleteFile("prueba.txt")
+print(statInfo)
